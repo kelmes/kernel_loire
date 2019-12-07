MotorolaMobilityLLC/kernel-msm

clk: qcom: Add support to log clock registers in case of failure
In case of PLL lock errors, RCGR configuration update failures,
and branch clock toggle timeouts, add support to capture the
clock registers as part of kernel logs.

Change-Id: Ifb0cefafc30f8796ba17f2d388fb65ed41aae485
Signed-off-by: Taniya Das <tdas@codeaurora.org>
Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

drivers/clk/qcom/clk-alpha-pll.c
@@ -16,6 +16,7 @@
 #include <linux/clk-provider.h>
 #include <linux/regmap.h>
 #include <linux/delay.h>
+#include <linux/sched/clock.h>
 
 #include "clk-alpha-pll.h"
 #include "common.h"
@@ -110,12 +111,16 @@ static int wait_for_pll(struct clk_alpha_pll *pll, u32 mask, bool inverse,
 	u32 val, off;
 	int count;
 	int ret;
-	const char *name = clk_hw_get_name(&pll->clkr.hw);
+	u64 time;
+	struct clk_hw *hw = &pll->clkr.hw;
+	const char *name = clk_hw_get_name(hw);
 
 	off = pll->offset;
 	ret = regmap_read(pll->clkr.regmap, off + PLL_MODE, &val);
 	if (ret)
 		return ret;
+
+	time = sched_clock();
 
 	for (count = 100; count > 0; count--) {
 		ret = regmap_read(pll->clkr.regmap, off + PLL_MODE, &val);
@@ -129,7 +134,12 @@ static int wait_for_pll(struct clk_alpha_pll *pll, u32 mask, bool inverse,
 		udelay(1);
 	}
 
-	WARN(1, "%s failed to %s!\n", name, action);
+	time = sched_clock() - time;
+
+	pr_err("PLL lock bit detection total wait time: %lld ns", time);
+
+	WARN_CLK(hw->core, name, 1, "failed to %s!\n", action);
+
 	return -ETIMEDOUT;
 }
 
@@ -499,7 +509,8 @@ static void print_pll_registers(struct seq_file *f, struct clk_hw *hw,
 	for (i = 0; i < size; i++) {
 		regmap_read(pll->clkr.regmap, pll->offset + pll_regs[i].offset,
 					&val);
-		seq_printf(f, "%20s: 0x%.8x\n", pll_regs[i].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n", pll_regs[i].name,
+					val);
 	}
 
 	regmap_read(pll->clkr.regmap, pll->offset + PLL_MODE, &val);
@@ -507,7 +518,8 @@ static void print_pll_registers(struct seq_file *f, struct clk_hw *hw,
 	if (val & PLL_FSM_ENA) {
 		regmap_read(pll->clkr.regmap, pll->clkr.enable_reg +
 					pll_vote_reg->offset, &val);
-		seq_printf(f, "%20s: 0x%.8x\n", pll_vote_reg->name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n",
+					pll_vote_reg->name, val);
 	}
 }
 

kelmes/kernel_loire

Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

drivers/clk/qcom/clk-alpha-pll.c
@@ -18,6 +18,7 @@
 #include <linux/clk-provider.h>
 #include <linux/regmap.h>
 #include <linux/delay.h>
+#include <linux/sched.h>
 
 #include "clk-alpha-pll.h"
 #include "common.h"
@@ -102,12 +103,16 @@ static int wait_for_pll(struct clk_alpha_pll *pll, u32 mask, bool inverse,
 	u32 val, off;
 	int count;
 	int ret;
-	const char *name = clk_hw_get_name(&pll->clkr.hw);
+	u64 time;
+	struct clk_hw *hw = &pll->clkr.hw;
+	const char *name = clk_hw_get_name(hw);
 
 	off = pll->offset;
 	ret = regmap_read(pll->clkr.regmap, off + PLL_MODE, &val);
 	if (ret)
 		return ret;
+
+	time = sched_clock();
 
 	for (count = 100; count > 0; count--) {
 		ret = regmap_read(pll->clkr.regmap, off + PLL_MODE, &val);
@@ -121,7 +126,12 @@ static int wait_for_pll(struct clk_alpha_pll *pll, u32 mask, bool inverse,
 		udelay(1);
 	}
 
-	WARN(1, "clk: %s failed to %s!\n", name, action);
+	time = sched_clock() - time;
+
+	pr_err("PLL lock bit detection total wait time: %lld ns", time);
+
+	WARN_CLK(hw->core, name, 1, "failed to %s!\n", action);
+
 	return -ETIMEDOUT;
 }
 
@@ -623,7 +633,8 @@ static void clk_alpha_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	for (i = 0; i < size; i++) {
 		regmap_read(pll->clkr.regmap, pll->offset + data[i].offset,
 					&val);
-		seq_printf(f, "%20s: 0x%.8x\n", data[i].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n", data[i].name,
+					val);
 	}
 
 	regmap_read(pll->clkr.regmap, pll->offset + data[0].offset, &val);
@@ -631,7 +642,8 @@ static void clk_alpha_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	if (val & PLL_FSM_ENA) {
 		regmap_read(pll->clkr.regmap, pll->clkr.enable_reg +
 					data1[0].offset, &val);
-		seq_printf(f, "%20s: 0x%.8x\n", data1[0].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n",
+					data1[0].name, val);
 	}
 }
 
@@ -870,7 +882,8 @@ static void clk_fabia_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	for (i = 0; i < size; i++) {
 		regmap_read(pll->clkr.regmap, pll->offset + data[i].offset,
 					&val);
-		seq_printf(f, "%20s: 0x%.8x\n", data[i].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n", data[i].name,
+					val);
 	}
 
 	regmap_read(pll->clkr.regmap, pll->offset + data[0].offset, &val);
@@ -878,7 +891,8 @@ static void clk_fabia_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	if (val & PLL_FSM_ENA) {
 		regmap_read(pll->clkr.regmap, pll->clkr.enable_reg +
 					data1[0].offset, &val);
-		seq_printf(f, "%20s: 0x%.8x\n", data1[0].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n",
+					data1[i].name, val);
 	}
 }
 
@@ -1375,7 +1389,8 @@ static void clk_trion_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	for (i = 0; i < size; i++) {
 		regmap_read(pll->clkr.regmap, pll->offset + data[i].offset,
 					&val);
-		seq_printf(f, "%20s: 0x%.8x\n", data[i].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n",
+					data[i].name, val);
 	}
 
 	regmap_read(pll->clkr.regmap, pll->offset + data[0].offset, &val);
@@ -1383,7 +1398,8 @@ static void clk_trion_pll_list_registers(struct seq_file *f, struct clk_hw *hw)
 	if (val & PLL_VOTE_FSM_ENA) {
 		regmap_read(pll->clkr.regmap, pll->clkr.enable_reg +
 					data1[0].offset, &val);
-		seq_printf(f, "%20s: 0x%.8x\n", data1[0].name, val);
+		clock_debug_output(f, false, "%20s: 0x%.8x\n",
+					data1[i].name, val);
 	}
 }
 